e1d11df9d6ba5666b64e1d3237ae8906c9a5791a
Ali  Abdou <abdouali422@gmail.com>
2022-10-01 18:55:47 +0100

    added rotations to the tree

--- utils/rb_tree.hpp
+++ utils/rb_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: aabdou <aabdou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 18:03:58 by aabdou            #+#    #+#             */
-/*   Updated: 2022/10/01 18:04:58 by aabdou           ###   ########.fr       */
+/*   Updated: 2022/10/01 18:53:33 by aabdou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,7 +105,43 @@ namespace ft {
 					node = node->right;
 				return node;
 			}
-			
+
+			void left_rotate(node_ptr node) {
+				node_ptr tmp = node->right; // save node's right branch
+				node->right = tmp->left; //node's new right child is tmp's old left child
+				if (tmp->left != _nil) // if tmp->left is not NULL
+					tmp->left->parent = node;
+				tmp->parent = node->parent; // tmp is the new node so it takes node's parent
+				if (node->parent == NULL)
+					this->_root = tmp;
+				else if (node == node->parent->left) // is node was its parents left child, tmp becomes its new parents left child
+					node->parent->left = tmp;
+				else
+					node->parent->right = tmp; // mirror case
+				tmp->left = node;
+				node->parent = tmp;
+
+			}
+
+			void right_rotate(node_ptr node) //mirror case
+			{
+				node_ptr tmp = node->left;
+
+				node->left = tmp->right;
+				if (tmp->right != _nil)
+					tmp->right->parent = node;
+				tmp->parent = node->parent;
+				if (node->parent == NULL)
+					this->_root = tmp;
+				else if (node == node->parent->left)
+					node->parent->left = tmp;
+				else
+					node->parent->right = tmp;
+				tmp->right = node;
+				node->parent = tmp;
+			}
+
+
 	};
 
 

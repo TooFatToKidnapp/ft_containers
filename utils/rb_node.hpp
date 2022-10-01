66f64e0fb3bd4cb1b8f4c6f7e0ebbb952e28b2b8
Ali  Abdou <abdouali422@gmail.com>
2022-10-01 16:10:05 +0100

    added new node structure and rb tree skelaton

--- utils/rb_node.hpp
+++ utils/rb_node.hpp
@@ -6,7 +6,7 @@
 /*   By: aabdou <aabdou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 17:56:28 by aabdou            #+#    #+#             */
-/*   Updated: 2022/09/30 18:46:43 by aabdou           ###   ########.fr       */
+/*   Updated: 2022/10/01 16:04:05 by aabdou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,52 +18,25 @@
 namespace ft {
 
 	enum rb_colour {
-		RED = false,
-		BLACK = true
+		RED,
+		BLACK
 	};
 
-
-	// base class that dosen't know about val "user data"
-	// used for sentinal node "NULL" to prevent unecessary memory usage
-	class rb_node_base {
+	template<class T>
+	class Node {
 		public:
-			rb_node_base*	left;
-			rb_node_base*	right;
-			rb_node_base*	parent;
-			rb_colour		colour;
+			typedef T	value_type;
 
-			// default constructor for sentinal
-			rb_node_base() : left(NULL), right(NULL) ,parent(NULL), colour(BLACK) {}
+			value_type	data;
 
-			// constructor with parent and children (for passing NULL)
-			rb_node_base(rb_node_base * parent, rb_node_base * child)
-				: left(child), right(child), parent(parent), colour(RED) {}
+			Node*		parent;
+			Node*		left;
+			Node*		right;
 
-			// copy constructor
-			rb_node_base(const rb_node_base &obj) {
-				this->parent = obj.parent;
-				this->left = obj.left;
-				this->right = obj.right;
-				this->colour = obj.colour;
-			}
-	};
+			rb_colour	colour;
 
-
-	// derived class that knows KEY (used for nodes with <key,T> data)
-	template<class Key>
-	class rb_node : public rb_node_base {
-		public:
-			// val is a pair <Key,T> obj
-			Key val;
-
-			// constructor with val and parent and child (NULL)
-			rb_node(const Key &val, rb_node_base* parent, rb_node_base* child)
-				: val(val), rb_node_base(parent, child) {}
-
-			rb_node(const rb_node_base &base) : rb_node_base(base) {}
-
-			rb_node(const rb_node& other) : rb_node_base(other) , val(other.val) {}
-
+			Node(value_type data, Node* parent, Node* left, Node* right, color clr)
+				: data(data), parent(parent), left(left), right(right), colour(clr) {}
 	};
 
 } // namespace ft
--- utils/rb_tree.hpp
+++ utils/rb_tree.hpp
@@ -6,14 +6,26 @@
 /*   By: aabdou <aabdou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 18:03:58 by aabdou            #+#    #+#             */
-/*   Updated: 2022/09/30 18:04:27 by aabdou           ###   ########.fr       */
+/*   Updated: 2022/10/01 16:08:49 by aabdou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RB_TREE
 #define RB_TREE
 
+#include "./rb_node.hpp"
 
+namespace ft {
+
+	template<class Key, class T, class get_key_from_val, class Compare = std::less<Key>, class Alloc = std::allocator<Node<T> > >
+	class RBT {
+
+		
+	};
+
+
+
+} // namespace ft
 
 
 

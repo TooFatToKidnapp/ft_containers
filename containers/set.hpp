e804f32a3517dbd07ac018444b809b8c4ddd616b
Ali  Abdou <abdouali422@gmail.com>
2022-10-11 15:30:01 +0100

    workin on ft::set

--- containers/set.hpp
+++ containers/set.hpp
@@ -6,15 +6,55 @@
 /*   By: aabdou <aabdou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 12:34:49 by aabdou            #+#    #+#             */
-/*   Updated: 2022/10/11 12:35:17 by aabdou           ###   ########.fr       */
+/*   Updated: 2022/10/11 15:28:59 by aabdou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SET_HPP
 #define SET_HPP
 
+#include <functional> // std::less<>
+#include <memory> // std::allocator
+#include "./../utils/rb_tree.hpp"
+#include "./../utils/rb_node.hpp"
+#include "./../utils/Bidirectional_Iterator.hpp"
 
+namespace ft
+{
+	template<class T, class Key>
+	class get_key_from_val : public std::unary_function<T, Key>
+	{
+		public:
+			const Key &operator()(const T &obj) const{
+				return obj;
+			}
+	};
 
+	template<class T, class Compare = std::less<T>, class Allocator = std::allocator<T> >
+	class set {
+		public:
+			typedef T											key_type;
+			typedef T											value_type;
+			typedef Compare										key_compare;
+			typedef Compare										value_compare;
+			typedef Allocator									allocator_type;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+
+			typedef ft::RBT<key_type, value_type, get_key_from_val<value_type, key_type>, key_compare> tree_type;
+			typedef ft::Node<value_type> node_type;
+			typedef ft::Node<value_type>* node_ptr;
+
+			typedef ft::BidirectionalIterator<value_type const, node_type const>	iterator;
+			typedef ft::BidirectionalIterator<value_type const, node_type const>	const_iterator;
+			typedef ft::reverse_iterator<iterator>									reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>							const_reverse_iterator;
+
+	};
+
+} // namespace ft
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "./containers/vector.hpp"
 #include "./containers/stack.hpp"
+#include "./containers/map.hpp"
+#include "./containers/set.hpp"
 #include "./utils/pair.hpp"
 #include "./utils/rb_tree.hpp"
 #include "./utils/Bidirectional_Iterator.hpp"
-#include "./containers/map.hpp"
 #include <list>
 #include <vector>
 #include <map>
+#include <set>
 #include <stack>
 #include <functional>
 #include <algorithm>
@@ -16,32 +18,13 @@ using ft::vector;
 using ft::stack;
 using ft::pair;
 using ft::map;
+using std::set;
 #ifndef somthing
 #define somthing
 #endif
 
-ft::pair<int ,std::string> make_pair(int i, std::string str) {
-	ft::pair<int, std::string> var(i, str);
-	return var;
-}
-
-
 int main() {
 
-	map<int, std::string> var;
-	var[7] = "hello";
-	var[-1] = "world";
-	var[1] = "somthing";
-	var[-111] = "xd";
-	var[878] = "test";
-
-	map<int, std::string> var2;
-	var2[77] = "fff";
-	var2[85] = "ppp";
-	var2[-5] = "rrr";
-	var2[0] = "zzz";
-
-	if (var > var2)
-		cout << "true\n";
+	set<int> var;
 
 }
